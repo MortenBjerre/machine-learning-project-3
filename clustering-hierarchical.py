670b752baa9357b0ba39ac52c37706bad97410bf
erick <erickastljensen@gmail.com>
2019-11-26 16:01:13 +0100

    cluster famhist

--- clustering-hierarchical.py
+++ clustering-hierarchical.py
@@ -1,20 +1,33 @@
-from dataextraction import X, X_standard, attributeNames
+from dataextraction import X, X_standard, attributeNames 
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-from pcavariance import (X, N, M, Y, U, S, V)
-
-import matplotlib.pyplot as plt
+from pcavariance import (N, M, Y, U, S, V)
 from toolbox_02450 import clusterplot
 from scipy.cluster.hierarchy import linkage, fcluster, dendrogram
 
 # Perform hierarchical/agglomerative clustering on data matrix
 ## single, complete, average, weighted, centroid, median, ward
-Method = 'ward'
+
+Method = 'ward' 
+#possibilities:
+#single
+#average
+#weighted
+#centroid
+#median
+#ward
+
 Metric = 'euclidean'
+#possibilities:
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.pdist.html#scipy.spatial.distance.pdist
+
+
 X = X_standard
 y = X[:,9] #make y the chd column
 X = X[:,:9] #get rid of chd column
+# y = X[:,4]
+# X = X[:,[0,1,2,3,5,6,7,8,9]]
+
 
 # PCA stuff so that clustering is graphed on PCA 2d projection
 Vt = V.T
@@ -35,7 +48,100 @@ for r in range(len(Z)):
 z0 = np.array(z0)
 z1 = np.array(z1)
 
-link = linkage(X, method=Method, metric=Metric)
+# link = linkage(X, method=Method, metric=Metric)
+
+# # Compute clusters by thresholding the dendrogram
+# Maxclust = 2
+# cls = fcluster(link, criterion='maxclust', t=Maxclust)
+
+# # Find out accuracy of clusters
+# a = cls.reshape(cls.shape[0],1)
+# truepos = 0
+# trueneg = 0
+# falsepos = 0
+# falseneg = 0
+# for index, item in enumerate(a):
+#     if item-1 == 0 and y[index] == 0:
+#         trueneg += 1
+#     elif item-1 == 1 and y[index] == 1:
+#         truepos += 1
+#     elif item-1 == 0 and y[index] == 1:
+#         falseneg += 1
+#     elif item-1 == 1 and y[index] == 0:
+#         falsepos += 1
+#     else:
+#         print("something weird", index, item)
+# print("Method:",Method)
+# print("truepos:", truepos)
+# print("trueneg:", trueneg)
+# print("falsepos:", falsepos)
+# print("falseneg:", falseneg)
+# print("Percent right:", (truepos + trueneg)/len(a) * 100)
+
+# # Plot clusters
+# plt.figure(1, figsize=(10,8))
+# clusterplot(Z[:,0:2], a, y=y)
+# plt.xlabel("PC1")
+# plt.ylabel("PC2")
+# plt.title("Hierarchical clustering")
+
+# # Display dendrogram
+# max_display_levels=6
+# plt.figure(2,figsize=(11,4))
+# dendrogram(link, truncate_mode='level', p=max_display_levels)
+
+# plt.show()
+
+#%% testing all posibilites 
+def testAll():
+    Maxclust = 2
+    methods = ["single","average","weighted","centroid","median","ward"]
+    metrics = ["euclidean","minkowski","cityblock","seuclidean","cosine","correlation","hamming","jaccard","chebyshev","canberra","braycurtis","mahalanobis","yule","matching","dice","kulsinski","regoerstanimoto","russellrao","sokalsneath","wminkowski"]
+
+    for method in methods:
+        for metric in metrics:
+            
+
+            # Compute clusters by thresholding the dendrogram
+            try:
+                
+                link = linkage(X, method=method, metric=metric)
+                cls = fcluster(link, criterion='maxclust', t=Maxclust)
+                print("Method used is",method)
+                print("Metric used is",metric,"\n")
+                # Find out accuracy of clusters
+                a = cls.reshape(cls.shape[0],1)
+                truepos = 0
+                trueneg = 0
+                falsepos = 0
+                falseneg = 0
+                for index, item in enumerate(a):
+                    if item-1 == 0 and y[index] == 0:
+                        trueneg += 1
+                    elif item-1 == 1 and y[index] == 1:
+                        truepos += 1
+                    elif item-1 == 0 and y[index] == 1:
+                        falseneg += 1
+                    elif item-1 == 1 and y[index] == 0:
+                        falsepos += 1
+                    else:
+                        print("something weird", index, item)
+                print("Method:",Method)
+                print("truepos:", truepos)
+                print("trueneg:", trueneg)
+                print("falsepos:", falsepos)
+                print("falseneg:", falseneg)
+                print("Percent right:", (truepos + trueneg)/len(a) * 100)
+                print("\n--------------------------------------------")
+            except:
+                pass
+            
+#%% new y is famhist to see if we can improve result
+X = X_standard
+y = X[:,4]
+X = X[:,[0,1,2,3,5,6,7,8,9]]
+
+link = linkage(X, method="average", metric="matching") #metric=hamming
 
 # Compute clusters by thresholding the dendrogram
 Maxclust = 2
@@ -57,7 +163,7 @@ for index, item in enumerate(a):
     elif item-1 == 1 and y[index] == 0:
         falsepos += 1
     else:
-        print("something weird", index, item)
+        print("something weird", index, item, y[index])
 print("Method:",Method)
 print("truepos:", truepos)
 print("trueneg:", trueneg)
@@ -66,12 +172,15 @@ print("falseneg:", falseneg)
 print("Percent right:", (truepos + trueneg)/len(a) * 100)
 
 # Plot clusters
-plt.figure(1)
+plt.figure(1, figsize=(10,8))
 clusterplot(Z[:,0:2], a, y=y)
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+plt.title("Hierarchical clustering for predicting famhist")
 
 # Display dendrogram
 max_display_levels=6
-plt.figure(2,figsize=(10,4))
+plt.figure(2,figsize=(11,4))
 dendrogram(link, truncate_mode='level', p=max_display_levels)
 
 plt.show()
