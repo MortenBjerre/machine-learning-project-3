7b443c36f723e50effb253d2a03102cba9763cb7
MortenBjerre <48279971+MortenBjerre@users.noreply.github.com>
2019-11-30 17:00:41 +0100

    gmm

--- clustering-GMM.py
+++ clustering-GMM.py
@@ -5,6 +5,9 @@ from scipy.io import loadmat
 from sklearn.mixture import GaussianMixture
 from sklearn import model_selection
 from dataextraction import X, X_standard, attributeNames
+from clusteringhierarchical import Z, z0,z1
+import matplotlib.pyplot as plt
+#from clustering-hierarchical import Z, z0, z1
 
 # Load Matlab data file and extract variables of interest
 X = X_standard
@@ -63,3 +66,41 @@ plot(KRange, 2*CVE,'-ok')
 legend(['BIC', 'AIC', 'Crossvalidation'])
 xlabel('K')
 show()
+#%%
+
+from toolbox_02450 import clusterplot
+covar_type = 'full'       # you can try out 'diag' as well
+reps = 3                  # number of fits with different initalizations, best result will be kept
+init_procedure = 'kmeans' # 'kmeans' or 'random'
+gmm = GaussianMixture(n_components=8, covariance_type=covar_type, 
+                              n_init=reps, init_params=init_procedure,
+                              tol=1e-6, reg_covar=1e-6).fit(X)
+clustering = gmm.predict(X)
+a = clustering.reshape(clustering.shape[0],1)
+plt.figure(1,figsize=(10,7))
+for i in range(8):
+    plt.scatter(Z[clustering == i][:,0],Z[clustering == i][:,1],label="cluster " + str(i))
+legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.title("GMM clustering")
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+plt.savefig("GMM-cluster-plot.png")
+plt.show()    
+    
+#3d
+def threedplot():
+    f = plt.figure()
+    ax = f.add_subplot(111, projection='3d')
+    for i in range(8):
+        #ax.scatter(z0[:,0], z0[:,1], z0[:,2], c='g', label = "chd=0")
+        #ax.scatter(z1[:,0], z1[:,1], z1[:,2], c='r', label = "chd=1")
+        ax.scatter(Z[clustering == i][:,0],Z[clustering == i][:,1],Z[clustering == i][:,2], label="cluster " + str(i))
+    
+    
+    
+    plt.legend()
+    plt.xlabel('PCA1')
+    plt.ylabel('PCA2')
+    ax.set_zlabel('PCA3')
+    plt.tight_layout()
+    plt.show()
